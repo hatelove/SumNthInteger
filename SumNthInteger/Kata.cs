f3741358b327b5449338a789012cc9e2298dae27
Joey Chen <jay.hatelove@gmail.com>
2018-01-04 20:53:24 +0800

    requirement re-define, pass test cases: 1. n is 0, 2. n is 1

--- SumNthInteger/Kata.cs
+++ SumNthInteger/Kata.cs
@@ -5,22 +5,25 @@ namespace SumNthInteger
 {
     internal class Kata
     {
-        private int[] numbers;
-
-        public Kata(int[] numbers)
+        public Kata()
         {
-            this.numbers = numbers;
         }
 
         public string SumNthInteger(int n, int maxVal)
         {
-            var count = Math.Abs(n);
-            var sum = numbers.Take(count).Sum();
+            var numberRange = GetNumbers(n);
+            var sum = numberRange.Sum();
             if (sum >= maxVal)
             {
                 return "input invalid";
             }
             return sum.ToString();
+        }
+
+        private int[] GetNumbers(int n)
+        {
+            var count = Math.Abs(n);
+            return Enumerable.Range(1, count).ToArray();
         }
     }
 }
--- SumNthInteger/UnitTest1.cs
+++ SumNthInteger/UnitTest1.cs
@@ -6,107 +6,24 @@ namespace SumNthInteger
     public class UnitTest1
     {
         [TestMethod]
-        public void N_larger_than_numbers_length_And_N_larger_than_0_sum_smaller_than_maxval()
+        public void new_n_is_0()
         {
-            var numbers = new[] { 4 };
-            var n = 3;
-            var maxVal = 20;
-            ResultShouldBe(numbers, n, maxVal, "4");
-        }
-
-        [TestMethod]
-        public void N_larger_than_numbers_length_And_N_larger_than_0_sum_larger_than_maxval()
-        {
-            var numbers = new[] { 40 };
-            var n = 3;
-            var maxVal = 20;
-            ResultShouldBe(numbers, n, maxVal, "input invalid");
-        }
-
-        [TestMethod]
-        public void N_larger_than_numbers_length_And_N_larger_than_0_sum_equal_to_maxval()
-        {
-            var numbers = new[] { 20 };
-            var n = 3;
-            var maxVal = 20;
-            ResultShouldBe(numbers, n, maxVal, "input invalid");
-        }
-
-        [TestMethod]
-        public void N_smaller_than_numbers_length()
-        {
-            var numbers = new[] { 4, 7, 5 };
-            var n = 2;
-            var maxVal = 20;
-            ResultShouldBe(numbers, n, maxVal, "11");
-        }
-
-        [TestMethod]
-        public void N_is_0()
-        {
-            var numbers = new[] { 4, 7, 5 };
             var n = 0;
-            var maxVal = 20;
-            ResultShouldBe(numbers, n, maxVal, "0");
+            var maxVal = 300;
+            ResultShouldBe(n, maxVal, "0");
         }
 
         [TestMethod]
-        public void N_smaller_than_0()
+        public void new_n_is_1()
         {
-            var numbers = new[] { 4, 7, 5 };
-            var n = -2;
-            var maxVal = 20;
-            ResultShouldBe(numbers, n, maxVal, "11");
+            var n = 1;
+            var maxVal = 300;
+            ResultShouldBe(n, maxVal, "1");
         }
 
-        [TestMethod]
-        public void numbers_exist_negative_number()
+        private void ResultShouldBe(int n, int maxVal, string expected)
         {
-            var numbers = new[] { 4, -7, 5 };
-            var n = 2;
-            var maxVal = 20;
-            ResultShouldBe(numbers, n, maxVal, "-3");
-        }
-
-        [TestMethod]
-        public void sum_and_maxval_both_negative()
-        {
-            var numbers = new[] { 4, -7, 5 };
-            var n = 2;
-            var maxVal = -20;
-            ResultShouldBe(numbers, n, maxVal, "input invalid");
-        }
-
-        [TestMethod]
-        public void sum_and_maxval_both_negative_and_sum_smaller_than_maxval()
-        {
-            var numbers = new[] { 4, -7, 5 };
-            var n = 2;
-            var maxVal = -2;
-            ResultShouldBe(numbers, n, maxVal, "-3");
-        }
-
-        [TestMethod]
-        public void numbers_is_empty()
-        {
-            var numbers = new int[] { };
-            var n = 2;
-            var maxVal = 20;
-            ResultShouldBe(numbers, n, maxVal, "0");
-        }
-
-        [TestMethod]
-        public void numbers_is_empty_and_maxval_is_negative()
-        {
-            var numbers = new int[] { };
-            var n = 2;
-            var maxVal = -3;
-            ResultShouldBe(numbers, n, maxVal, "input invalid");
-        }
-
-        private static void ResultShouldBe(int[] numbers, int n, int maxVal, string expected)
-        {
-            Assert.AreEqual(expected, new Kata(numbers).SumNthInteger(n, maxVal));
+            Assert.AreEqual(expected, new Kata().SumNthInteger(n, maxVal));
         }
     }
 }
